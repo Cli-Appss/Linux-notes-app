97927c8e8b2e54bd783e075f93a2baf0a6361634
Udhay <57910021+code123841@users.noreply.github.com>
2021-10-22 19:09:34 +0530

    help.sh update

--- help.sh
+++ help.sh
@@ -5,4 +5,4 @@ echo '-h Shows all the Command to use this program'
 echo ''
 echo "-a to Setup the aliases"
 echo ''
-echo ""
+echo "-web lauches the web cli for this package (still under beta changes)"
