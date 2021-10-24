5fa30a9baa433e289cecca781cb2a65890acc89c
Udhay <57910021+code123841@users.noreply.github.com>
2021-10-24 11:01:36 +0530

    fix fr mkdir.sh

--- mkdir.sh
+++ mkdir.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 mkdir lib
 cd lib && mkdir notes
+
+FILE= lib/notes
+
+
+if [[ -f "$FILE" ]]; then
+    echo "$FILE exists."
+fi
--- noteAdd.sh
+++ noteAdd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 read -p 'Please enter Filename>' file
-cd notes
+cd lib/notes
 nano $file.txt
 echo ''
 echo 'Created Note successfully'
