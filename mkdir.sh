10afdff229ba89b21b489d211129c2de86cae373
Udhay <57910021+code123841@users.noreply.github.com>
2021-10-24 10:57:41 +0530

    Update mkdir.sh

--- mkdir.sh
+++ mkdir.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-FILE = lib/notes
-
-if test -f "$FILE"; then 
-    echo './notes File already exists posting notes in the notes directory'
-else
-    echo 'Creating /lib/notes directory'
-    
-fi
+mkdir lib
