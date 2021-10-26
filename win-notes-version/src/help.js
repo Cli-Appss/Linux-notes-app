8c0ee8ca49010c415e82373a33c8853f27a71b7f
Udhay <57910021+code123841@users.noreply.github.com>
2021-10-26 11:17:08 +0530

    cutomizing with chalk cololrs

--- win-notes-version/src/help.js
+++ win-notes-version/src/help.js
@@ -1,8 +1,10 @@
+const chalk = require('chalk');
+
 export async function help(){
     var Helpcommand = "Usage: command[-h] Shows you the defination of the Command";
     var noteCommand1 = "Note: [-v] Shows version of the of the app"
     
-    console.log(Helpcommand)
+    console.log(chalk.blue(Helpcommand))
     console.log("")
     console.log(noteCommand1)
 }
