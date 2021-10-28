f81bd3e5abf99aab5e0e09e8a0594a985bdd47b7
Udhay <57910021+code123841@users.noreply.github.com>
2021-10-28 19:59:51 +0530

    Update task-remoce.sh

--- task-remove.sh
+++ task-remove.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 read -p 'Please enter Filename you want to delete>' file
-
+cd tasks/
 
 if [ -f "$file" ]
 then
-    cd tasks/
-    rm $file.txt
+    rm $file
     echo 'removed note successfully'
 elif [ ! -f "$file" ]
 then 
-    echo "file was not found in $file"
+    echo "file was not found in $file please try again with .txt format"
 fi
 
