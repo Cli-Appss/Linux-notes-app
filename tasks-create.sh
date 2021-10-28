b2551aa2c600256607984a979aefcf24d1ea94f5
Udhay <57910021+code123841@users.noreply.github.com>
2021-10-28 19:04:51 +0530

    update note-task.sh

--- tasks-create.sh
+++ tasks-create.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
 echo 'Creating tasks/ folder'
+
+dirname="tasks/"
+
+if [ ! -d "$dirname" ]
+then
+    echo "File doesn't exist. Creating now"
+    mkdir ./$dirname
+    echo "File created"
+else
+    echo "NOTICE:File exists in correct path skipping.."
+fi
+
 sleep 1 
 
+
+
+echo 'successfully created the following directories..'
